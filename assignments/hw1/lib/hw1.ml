fe493b6cd342740a11dc1030420b1f0d1566aa2f
Andras Zolyomi <andras.zolyomi@gmail.com>
2023-05-01 15:27:12 -0700

    yay

--- assignments/hw1/lib/hw1.ml
+++ assignments/hw1/lib/hw1.ml
@@ -4,18 +4,197 @@ open Hw1_sig
 let todo () = failwith "Your code here"
 
 module Listy : LISTY = struct
-  let prepend (x : 'a) (xs : 'a list) : 'a list = todo ()
-  let append (x : 'a) (xs : 'a list) : 'a list = todo ()
-  let cat (xs : 'a list) (ys : 'a list) : 'a list = todo ()
-  let zip (xs : 'a list) (ys : 'b list) : ('a * 'b) list option = todo ()
-  let permute (xs : 'a list) : 'a list list = todo ()
+  let prepend (x : 'a) (xs : 'a list) : 'a list = x::xs
+  let rec append (x : 'a) (xs : 'a list) : 'a list = 
+    match xs with
+    [] -> [x]
+    | h::t -> h::append x t;;
+  let rec cat (xs : 'a list) (ys : 'a list) : 'a list = 
+    match ys with
+    [] -> xs
+    | h::t -> cat (append h xs) t
+  let rec zip (xs : 'a list) (ys : 'b list) : ('a * 'b) list option = 
+    match xs with
+    [] -> (
+      match ys with
+      [] -> Some([])
+      | h::t -> None
+    )
+    | h1::t1 -> (
+      match ys with
+      [] -> None
+      | h2::t2 -> (
+        let z = (zip t1 t2) in
+        match z with
+        None -> None
+        | Some(x) -> Some((h1, h2)::x)
+      )
+      )
+
+  (* map from the slides *)
+  let rec map (f : 'a -> 'b) (xs : 'a list) : 'b list = 
+    match xs with
+    [] -> []
+    | h::t -> (f h)::(map f t)
+
+  (* helper function to take element x and add it to every position in list ys, yielding a list of lists *)
+  let rec insert_all (x: 'a) (ys: 'a list) : 'a list list =
+    match ys with
+    [] -> [[x]] (* ys empty -> only one pos to add *)
+    | h::t -> (* ys not empty - add to beginning of list AND beginning of result of insert_all*)
+      (x::ys) :: (map (prepend h) (insert_all x t))
+
+  (* recursive function to take a N-D array and return N-1-D array. *)
+  let rec flatten (xs : 'a list list) : 'a list = 
+    match xs with
+    [] -> []
+    | h::t -> cat h (flatten t)
+    
+  (* TODO: TEST RIGOROUSLY! *)
+  let rec permute (xs : 'a list) : 'a list list = 
+    match xs with
+    [] -> [[]]
+    | h::t -> (
+      let p = permute t in
+      (* p is a list of lists *)
+      (* for each list, insert h in every spot. Works only because elements must be unique. *)
+      let map_insert_all_result = map (insert_all h) p in
+      (* map_insert_all is a list of lists of lists *)
+      (* flatten it to just a list of lists *)
+      flatten map_insert_all_result
+    )
+    
 end
 
 module Treey : TREEY = struct
-  let skeleton (t : 'a tree) : unit tree = todo ()
-  let selfie (t : 'a tree) : 'a tree tree = todo ()
-  let timestamp (t : 'a tree) : (int * 'a) tree = todo ()
-  let accepts (w : word) (t : tree_nfa) : bool = todo ()
-  let lookup (key : word) (t : 'a trie) : 'a option = todo ()
-  let insert (key : word) (value : 'a) (t : 'a trie) : 'a trie = todo ()
+  let rec skeleton (t : 'a tree) : unit tree = 
+    match t with
+    Leaf -> Leaf
+    | Node (v, l, r) -> node () (skeleton l) (skeleton r)
+  
+  let rec selfie_rec (copy: 'a tree) (t : 'a tree) : 'a tree tree = 
+    match t with
+    Leaf -> Leaf
+    | Node (v, l, r) -> node copy (selfie_rec copy l) (selfie_rec copy r)
+  let selfie (t : 'a tree) : 'a tree tree = 
+    selfie_rec t t
+
+  let timestamp (t : 'a tree) : (int * 'a) tree =
+    (* timestamp_rec should return a ((int * 'a) tree * int) with root value (n, val) paired with finish value n_done
+       n_done = the last labeled number in the dfs of subtree. It's important to return,
+       so that we can use it in computation of RHS to not have duplicate values. *)
+
+    let rec timestamp_rec (t : 'a tree) (n : int) : (int * 'a) tree * int = 
+      match t with
+      (* If leaf node, shouldn't affect count. *)
+      Leaf -> (Leaf, n)
+      (* Otherwise, compute (l_done, n1) and (r_done, n2) sequentially
+         Return (Node (n, v), l_done, r_done), n2 *)
+      | Node (v, l, r) -> 
+        let (l_done, n1) = timestamp_rec l (n+1) in
+        let (r_done, n2) = timestamp_rec r (n1) in
+        let finished_node = Node ((n, v), l_done, r_done) in
+        (finished_node, (n2))
+    in 
+    let (t_done, n_done) = timestamp_rec t 0 in
+    t_done
+
+  let rec accepts (w : word) (t : tree_nfa) : bool = 
+    match w with
+    (* word is empty -> epsilon *)
+    [] -> (
+      match t with
+      (* tree root is leaf *)
+      Leaf -> false
+      (* tree root is node *)
+      | _ -> true
+    )
+    (* word has at least one letter
+       rest of the word (could be []) stored in w' *)
+    | letter::w' -> (
+      match t with
+      (* tree root is leaf *)
+      Leaf -> false
+      (* tree root is node *)
+      | Node (x, l, r) -> (
+        (* get next tree based on transition *)
+        let next_tree =
+          match letter with
+          | O -> l
+          | I -> r
+        in 
+        (* check if letter accepts *)
+        let letter_accepts = 
+          match next_tree with
+          | Leaf -> false
+          | _ -> true
+        in
+        (* if letter accepts and recursion accepts on next tree, return true. else false (AND)*)
+        letter_accepts && accepts (w') (next_tree)
+      )
+    )
+
+  let rec lookup (key : word) (t : 'a trie) : 'a option = 
+    match key with
+    (* word is empty -> epsilon *)
+    [] -> (
+      match t with
+      (* tree root is leaf *)
+      Leaf -> None
+      (* tree root is node with None or Some x *)
+      | Node (x, l, r) -> x
+    )
+    (* word has at least one letter
+       rest of the word (could be []) stored in w' *)
+    | letter::w' -> (
+      match t with
+      (* tree root is leaf *)
+      Leaf -> None
+      (* tree root is node with None or Some x *)
+      | Node (x, l, r) -> (
+        (* get next tree based on transition *)
+        let next_tree =
+          match letter with
+          | O -> l
+          | I -> r
+        in 
+        (* check if letter accepts *)
+        let letter_accepts = 
+          match next_tree with
+          | Leaf -> false
+          | _ -> true
+        in
+        (* if letter accepts and recursion accepts on next tree, return true. else false (AND)*)
+        match letter_accepts with
+        true -> lookup (w') (next_tree)
+        | false -> None
+      )
+    )
+  let rec insert (key : word) (value : 'a) (t : 'a trie) : 'a trie = 
+    match key with
+    (* no word -> replace root *)
+    [] -> (
+      match t with
+      (* if leaf, make new leafy node with value *)
+      Leaf -> node' (Some value)
+      (* else just change value *)
+      | Node (x, l, r) -> node (Some value) l r
+    )
+    (* some letter followed by subsequent word (could be []) *)
+    | letter::key' -> (
+      match t with
+      (* if tree is leaf, make new node and insert on appropriate branch based on letter *)
+      Leaf -> (
+        let Node (_, l, r) = node' None in
+        match letter with
+        O -> node None (insert key' value t) r
+        | I -> node None l (insert key' value t)
+      )
+      (* else tree is node, insert on appropriate branch based on letter *)
+      | Node (x, l, r) -> (
+        match letter with
+        O -> node x (insert key' value l) r
+        | I -> node x l (insert key' value r)
+      ) 
+    )
 end
--- assignments/hw1/test/hw1_test.ml
+++ assignments/hw1/test/hw1_test.ml
@@ -5,26 +5,85 @@ open Treey
 
 (* The tests for Listy *)
 let test_prepend () =
-  Alcotest.(check (list int)) "same lists" [ 1; 2; 3 ] (prepend 1 [ 2; 3 ])
+  (* normal test *)
+  Alcotest.(check (list int)) "same lists" [ 1; 2; 3 ] (prepend 1 [ 2; 3 ]);;
+  
+  (* empty list *)
+  Alcotest.(check (list int)) "same lists" [ 1] (prepend 1 [ ])
 
 let test_append () =
-  Alcotest.(check (list int)) "same lists" [ 2; 3; 1 ] (append 1 [ 2; 3 ])
+  (* normal test *)
+  Alcotest.(check (list int)) "same lists" [ 2; 3; 1 ] (append 1 [ 2; 3 ]);;
+  
+  (* empty list *)
+  Alcotest.(check (list int)) "same lists" [ 2] (append 2 [ ])
 
 let test_cat () =
+  (* normal test *)
   Alcotest.(check (list int))
     "same lists" [ 1; 2; 3; 4 ]
-    (cat [ 1; 2 ] [ 3; 4 ])
+    (cat [ 1; 2 ] [ 3; 4 ]);;
+
+  (* empty second list *)
+  Alcotest.(check (list int))
+    "same lists" [ 1; 2]
+    (cat [ 1; 2 ] []);;
+
+  (* empty first list *)
+  Alcotest.(check (list int))
+    "same lists" [ 1; 2; 3; 4]
+    (cat [] [ 1; 2; 3; 4]);;
+
+  (* both lists empty *)
+  Alcotest.(check (list int))
+    "same lists" [ ]
+    (cat [] [ ]);;
+
+  (* different size lists *)
+  Alcotest.(check (list int))
+    "same lists" [ 1; 4; 2; 3; 5]
+    (cat [1; 4] [2; 3; 5]);;
+  
 
 let test_zip () =
+  (* normal test *)
   Alcotest.(check (option (list (pair int string))))
     "same lists"
     (Some [ (1, "a"); (2, "b") ])
-    (zip [ 1; 2 ] [ "a"; "b" ])
+    (zip [ 1; 2 ] [ "a"; "b" ]);;
+
+  (* more elements test *)
+  Alcotest.(check (option (list (pair int string))))
+    "same lists"
+    (Some [ (1, "a"); (2, "b"); (1, "c"); (1, "a"); (5, "e")])
+    (zip [ 1; 2; 1; 1; 5 ] [ "a"; "b"; "c"; "a"; "e" ]);;
+
+  (* both empty list *)
+  Alcotest.(check (option (list (pair int string))))
+    "same lists"
+    (Some [ ])
+    (zip [ ] [ ]);;
 
 let test_zip_fail () =
+  (* normal test *)
   Alcotest.(check (option (list (pair int string))))
     "same lists" None
-    (zip [ 1 ] [ "a"; "b" ])
+    (zip [ 1 ] [ "a"; "b" ]);;
+
+  (* more elements *)
+  Alcotest.(check (option (list (pair int string))))
+    "same lists" None
+    (zip [ 1; 5; 1; 2; 1] [ "a"; "b"; "d" ]);;
+
+  (* first list empty *)
+  Alcotest.(check (option (list (pair int string))))
+    "same lists" None
+    (zip [ ] [ "a"; "b" ]);;
+
+  (* second list empty *)
+  Alcotest.(check (option (list (pair int string))))
+    "same lists" None
+    (zip [ 1; 3; 4 ] [ ]);;
 
 let rec cmp_aph x y =
   match (x, y) with
@@ -33,6 +92,7 @@ let rec cmp_aph x y =
   | _, _ -> 0
 
 let test_permute () =
+  (* normal test *)
   Alcotest.(check (list (list int)))
     "same lists"
     [
@@ -43,11 +103,72 @@ let test_permute () =
       [ 3; 1; 2 ];
       [ 3; 2; 1 ];
     ]
-    (List.sort cmp_aph (permute [ 1; 2; 3 ]))
+    (List.sort cmp_aph (permute [ 1; 2; 3 ]));;
 
+  (* empty list *)
+  Alcotest.(check (list (list int)))
+    "same lists"
+    [
+      []
+    ]
+    (List.sort cmp_aph (permute [ ]));;
+
+  (* one element *)
+  Alcotest.(check (list (list int)))
+    "same lists"
+    [
+      [ 1 ];
+    ]
+    (List.sort cmp_aph (permute [ 1 ]));;
+
+  (* two elements *)
+  Alcotest.(check (list (list int)))
+    "same lists"
+    [
+      [ 1; 2 ];
+      [ 2; 1 ];
+    ]
+    (List.sort cmp_aph (permute [ 1; 2 ]));;
+
+  (* four elements *)
+  Alcotest.(check (list (list int)))
+    "same lists"
+    [
+      [ 1; 2; 3; 4];
+      [ 1; 2; 4; 3];
+      [ 1; 3; 2; 4];
+      [ 1; 3; 4; 2];
+      [ 1; 4; 2; 3];
+      [ 1; 4; 3; 2];
+
+      [ 2; 1; 3; 4];
+      [ 2; 1; 4; 3];
+      [ 2; 3; 1; 4];
+      [ 2; 3; 4; 1];
+      [ 2; 4; 1; 3];
+      [ 2; 4; 3; 1];
+      
+      [ 3; 1; 2; 4];
+      [ 3; 1; 4; 2];
+      [ 3; 2; 1; 4];
+      [ 3; 2; 4; 1];
+      [ 3; 4; 1; 2];
+      [ 3; 4; 2; 1];
+
+      [ 4; 1; 2; 3];
+      [ 4; 1; 3; 2];
+      [ 4; 2; 1; 3];
+      [ 4; 2; 3; 1];
+      [ 4; 3; 1; 2];
+      [ 4; 3; 2; 1];
+    ]
+    (List.sort cmp_aph (permute [ 1; 2; 3; 4 ]));;
+    
+    
 (* The tests for Treey *)
 
 let test_skeleton () =
+  (* normal test *)
   Alcotest.(check string)
     "same trees"
     (let t1 = node' () in
@@ -61,9 +182,22 @@ let test_skeleton () =
      let t3 = node 1 t1 t2 in
      let t4 = node' 4 in
      let t5 = node 0 t3 t4 in
-     [%derive.show: unit tree] (skeleton t5))
+     [%derive.show: unit tree] (skeleton t5));;
+
+  (* node no children *)
+  Alcotest.(check string)
+    "same trees"
+    (let t1 = node' () in [%derive.show: unit tree] t1)
+    ([%derive.show: unit tree] (skeleton (node' 5)));;
+
+  (* leaf *)
+  Alcotest.(check string)
+    "same trees"
+    ([%derive.show: unit tree] leaf)
+    ([%derive.show: unit tree] (skeleton leaf));;
 
 let test_selfie () =
+  (* normal test *)
   Alcotest.(check string)
     "same trees"
     (let t1 = node' 1 in
@@ -76,9 +210,32 @@ let test_selfie () =
     (let t1 = node' 1 in
      let t2 = node' 2 in
      let t3 = node 0 t1 t2 in
-     [%derive.show: int tree tree] (selfie t3))
+     [%derive.show: int tree tree] (selfie t3));;
+  
+  (* node no children *)
+  Alcotest.(check string)
+    "same trees"
+    (let t1 = node' (node' 5) in [%derive.show: int tree tree] t1)
+    ([%derive.show: int tree tree] (selfie (node' 5)));;
+
+  (* leaf *)
+  Alcotest.(check string)
+    "same trees"
+    ([%derive.show: int tree tree] leaf)
+    ([%derive.show: int tree tree] (selfie leaf));;
+
+let dfs_eg = 
+  let t1 = node' "f" in 
+  let t2 = node "e" t1 leaf in 
+  let t3 = node "d" t2 leaf in 
+  let t4 = node "c" t3 leaf in 
+  let t5 = node "b" t4 leaf in 
+  let t6 = node' "g" in 
+  let t7 = node "a" t5 t6 in 
+  t7
 
 let test_timestamp () =
+  (* normal test *)
   Alcotest.(check string)
     "same trees"
     (let t1 = node' (2, "b") in
@@ -94,7 +251,70 @@ let test_timestamp () =
      let t4 = node' "r" in
      let t5 = node "m" t3 t4 in
      let t6 = node "n" t2 t5 in
-     [%derive.show: (int * string) tree] (timestamp t6))
+     [%derive.show: (int * string) tree] (timestamp t6));;
+
+  (* node no children *)
+  Alcotest.(check string)
+    "same trees"
+    (let t1 = node' (0,()) in [%derive.show: (int * unit) tree] t1)
+    ([%derive.show: (int * unit) tree] (timestamp (node'())));;
+
+  (* leaf *)
+  Alcotest.(check string)
+    "same trees"
+    ([%derive.show: (int * unit) tree] leaf)
+    ([%derive.show: (int * unit) tree] (timestamp leaf));;
+
+  (* deep tree *)
+  Alcotest.(check string)
+    "same trees"
+    (let t1 = node' (5, "f") in 
+    let t2 = node (4, "e") t1 leaf in 
+    let t3 = node (3, "d") t2 leaf in 
+    let t4 = node (2, "c") t3 leaf in 
+    let t5 = node (1, "b") t4 leaf in 
+    let t6 = node' (6, "g") in 
+    let t7 = node (0, "a") t5 t6 in 
+    [%derive.show: (int * string) tree] t7)
+    ([%derive.show: (int * string) tree] (timestamp dfs_eg));;
+
+  
+let test_nfa () = 
+  (* node no children *)
+  Alcotest.(check bool) "same bool" true (accepts [ ] (node' ()));;
+  Alcotest.(check bool) "same bool" false (accepts [ O ] (node' ()));;
+  Alcotest.(check bool) "same bool" false (accepts [ I ] (node' ()));;
+  Alcotest.(check bool) "same bool" false (accepts [ O; O ] (node' ()));;
+  Alcotest.(check bool) "same bool" false (accepts [ O; I ] (node' ()));;
+  Alcotest.(check bool) "same bool" false (accepts [ I; O ] (node' ()));;
+  Alcotest.(check bool) "same bool" false (accepts [ I; I ] (node' ()));;
+
+  (* leaf *)
+  Alcotest.(check bool) "same bool" false (accepts [ ] leaf);;
+  Alcotest.(check bool) "same bool" false (accepts [ O ] leaf);;
+  Alcotest.(check bool) "same bool" false (accepts [ I ] leaf);;
+  Alcotest.(check bool) "same bool" false (accepts [ O; O ] leaf);;
+  Alcotest.(check bool) "same bool" false (accepts [ O; I ] leaf);;
+  Alcotest.(check bool) "same bool" false (accepts [ I; O ] leaf);;
+  Alcotest.(check bool) "same bool" false (accepts [ I; I ] leaf);;
+
+  (* dfs tree *)
+  Alcotest.(check bool) "same bool" true (accepts [ ] (skeleton dfs_eg));;
+  Alcotest.(check bool) "same bool" true (accepts [ I ] (skeleton dfs_eg));;
+  Alcotest.(check bool) "same bool" false (accepts [ I; O ] (skeleton dfs_eg));;
+  Alcotest.(check bool) "same bool" false (accepts [ I; I ] (skeleton dfs_eg));;
+  Alcotest.(check bool) "same bool" true (accepts [ O ] (skeleton dfs_eg));;
+  Alcotest.(check bool) "same bool" true (accepts [ O; O ] (skeleton dfs_eg));;
+  Alcotest.(check bool) "same bool" false (accepts [ O; I ] (skeleton dfs_eg));;
+  Alcotest.(check bool) "same bool" true (accepts [ O; O; O ] (skeleton dfs_eg));;
+  Alcotest.(check bool) "same bool" false (accepts [ O; O; I ] (skeleton dfs_eg));;
+  Alcotest.(check bool) "same bool" true (accepts [ O; O; O; O] (skeleton dfs_eg));;
+  Alcotest.(check bool) "same bool" false (accepts [ O; O; O; I ] (skeleton dfs_eg));;
+  Alcotest.(check bool) "same bool" true (accepts [ O; O; O; O; O ] (skeleton dfs_eg));;
+  Alcotest.(check bool) "same bool" false (accepts [ O; O; O; O; I] (skeleton dfs_eg));;
+  Alcotest.(check bool) "same bool" false (accepts [ O; O; O; O; O; O ] (skeleton dfs_eg));;
+  Alcotest.(check bool) "same bool" false (accepts [ O; O; O; O; O; I ] (skeleton dfs_eg));;
+  Alcotest.(check bool) "same bool" false (accepts [ I; I; I; O] (skeleton dfs_eg));;
 
 let nfa_eg =
   let t1 = node' () in
@@ -109,6 +329,53 @@ let test_accepts_II () =
 let test_accepts_OO () =
   Alcotest.(check bool) "same bool" false (accepts [ O; O ] nfa_eg)
 
+let option_dfs_eg = 
+  let t1 = node' (Some "f") in 
+  let t2 = node (Some "e") t1 leaf in 
+  let t3 = node (Some "d") t2 leaf in 
+  let t4 = node (Some "c") t3 leaf in 
+  let t5 = node (Some "b") t4 leaf in 
+  let t6 = node' (Some "g") in 
+  let t7 = node (Some "a") t5 t6 in 
+  t7
+
+let test_lookup () = 
+  (* node no children *)
+  Alcotest.(check (option string)) "same option string" (Some "a") (lookup [ ] (node' (Some "a")));;
+  Alcotest.(check (option string)) "same option string" None (lookup [ O ] (node' (Some "a")));;
+  Alcotest.(check (option string)) "same option string" None (lookup [ I ] (node' (Some "a")));;
+  Alcotest.(check (option string)) "same option string" None (lookup [ O; O ] (node' (Some "a")));;
+  Alcotest.(check (option string)) "same option string" None (lookup [ O; I ] (node' (Some "a")));;
+  Alcotest.(check (option string)) "same option string" None (lookup [ I; O ] (node' (Some "a")));;
+  Alcotest.(check (option string)) "same option string" None (lookup [ I; I ] (node' (Some "a")));;
+
+  (* leaf *)
+  Alcotest.(check (option string)) "same option string" None (lookup [ ] leaf);;
+  Alcotest.(check (option string)) "same option string" None (lookup [ O ] leaf);;
+  Alcotest.(check (option string)) "same option string" None (lookup [ I ] leaf);;
+  Alcotest.(check (option string)) "same option string" None (lookup [ O; O ] leaf);;
+  Alcotest.(check (option string)) "same option string" None (lookup [ O; I ] leaf);;
+  Alcotest.(check (option string)) "same option string" None (lookup [ I; O ] leaf);;
+  Alcotest.(check (option string)) "same option string" None (lookup [ I; I ] leaf);;
+
+  (* dfs tree *)
+  Alcotest.(check (option string)) "same option string" (Some "a") (lookup [ ] ( option_dfs_eg));;
+  Alcotest.(check (option string)) "same option string" (Some "g") (lookup [ I ] ( option_dfs_eg));;
+  Alcotest.(check (option string)) "same option string" None (lookup [ I; O ] (option_dfs_eg));;
+  Alcotest.(check (option string)) "same option string" None (lookup [ I; I ] (option_dfs_eg));;
+  Alcotest.(check (option string)) "same option string" (Some "b") (lookup [ O ] (option_dfs_eg));;
+  Alcotest.(check (option string)) "same option string" (Some "c") (lookup [ O; O ] (option_dfs_eg));;
+  Alcotest.(check (option string)) "same option string" None (lookup [ O; I ] (option_dfs_eg));;
+  Alcotest.(check (option string)) "same option string" (Some "d") (lookup [ O; O; O ] (option_dfs_eg));;
+  Alcotest.(check (option string)) "same option string" None (lookup [ O; O; I ] (option_dfs_eg));;
+  Alcotest.(check (option string)) "same option string" (Some "e") (lookup [ O; O; O; O] (option_dfs_eg));;
+  Alcotest.(check (option string)) "same option string" None (lookup [ O; O; O; I ] (option_dfs_eg));;
+  Alcotest.(check (option string)) "same option string" (Some "f") (lookup [ O; O; O; O; O ] (option_dfs_eg));;
+  Alcotest.(check (option string)) "same option string" None (lookup [ O; O; O; O; I] (option_dfs_eg));;
+  Alcotest.(check (option string)) "same option string" None (lookup [ O; O; O; O; O; O ] (option_dfs_eg));;
+  Alcotest.(check (option string)) "same option string" None (lookup [ O; O; O; O; O; I ] (option_dfs_eg));;
+  Alcotest.(check (option string)) "same option string" None (lookup [ I; I; I; O] (option_dfs_eg));;
+
 let trie_eg =
   let t1 = node' (Some 5) in
   let t2 = node' (Some 4) in
@@ -117,10 +384,10 @@ let trie_eg =
   t4
 
 let test_lookup_OO () =
-  Alcotest.(check (option int)) "same bool" None (lookup [ O; O ] trie_eg)
+  Alcotest.(check (option int)) "same option string" None (lookup [ O; O ] trie_eg)
 
 let test_lookup_II () =
-  Alcotest.(check (option int)) "same bool" (Some 4) (lookup [ I; I ] trie_eg)
+  Alcotest.(check (option int)) "same option string" (Some 4) (lookup [ I; I ] trie_eg)
 
 let trie_inserted =
   let t0 = node' (Some 6) in
@@ -130,11 +397,45 @@ let trie_inserted =
   let t4 = node (Some 3) t1 t3 in
   t4
 
+let insert_dfs_eg = 
+  let t1 = node' (Some 6) in 
+  let t2 = node (Some 5) t1 leaf in 
+  let t3 = node (Some 4) t2 leaf in 
+  let t4 = node (Some 3) t3 leaf in 
+  let t5 = node (Some 2) t4 leaf in 
+  let t6 = node' (Some 7) in 
+  let t7 = node (Some 1) t5 t6 in 
+  t7
+
+
+
 let test_insert () =
+  (* normal test *)
   Alcotest.(check string)
     "same trie"
     ([%derive.show: int trie] trie_inserted)
-    ([%derive.show: int trie] (insert [ O; O ] 6 trie_eg))
+    ([%derive.show: int trie] (insert [ O; O ] 6 trie_eg));;
+
+  (* start from node no children *)
+    Alcotest.(check string)
+    "same trie"
+    ([%derive.show: int trie] (node (Some 0) leaf (node None (node None leaf (node None leaf (node' (Some 14)))) leaf)))
+    ([%derive.show: int trie] (insert [ I; O; I; I ] 14 (node' (Some 0))));;
+  
+
+  (* start from leaf *)
+  Alcotest.(check string)
+    "same trie"
+    ([%derive.show: int trie] (node None (node None (node None leaf (node None leaf (node' (Some 14)))) leaf) leaf))
+    ([%derive.show: int trie] (insert [ O; O; I; I ] 14 leaf));;
+
+
+  (* make dfs tree from scratch *)
+  Alcotest.(check string)
+    "same trie"
+    ([%derive.show: int trie] insert_dfs_eg)
+    ([%derive.show: int trie] (insert [O; O] 3 (insert [O] 2 (insert [O; O; O; O] 5 (insert [] 1 (insert [O; O; O] 4 (insert [ O; O; O; O; O ] 6 (insert [I] 7 leaf))))))) );;
+  
 
 (* Run it *)
 let () =
@@ -150,8 +451,10 @@ let () =
       ("skeleton", [ test_case "skeleton" `Slow test_skeleton ]);
       ("selfie", [ test_case "selfie" `Slow test_selfie ]);
       ("timestamp", [ test_case "timestamp" `Slow test_timestamp ]);
+      ("test-nfa", [ test_case "test-nfa" `Slow test_nfa ]);
       ("accepts-II", [ test_case "accepts-II" `Slow test_accepts_II ]);
       ("accepts-OO", [ test_case "accepts-OO" `Slow test_accepts_OO ]);
+      ("test-lookup", [ test_case "test-lookup" `Slow test_lookup ]);
       ("lookup-II", [ test_case "lookup-II" `Slow test_lookup_II ]);
       ("lookup-OO", [ test_case "lookup-OO" `Slow test_lookup_OO ]);
       ("insert", [ test_case "insert" `Slow test_insert ]);
